7de77980498e5f71470cf05fc9c42f72cc829065
jeffzhou95 <xz6429@utexas.edu>
2018-12-28 17:16:29 -0600

    Finishing driving system.

--- BattleTank/Source/BattleTank/Public/SprungWheel.h
+++ BattleTank/Source/BattleTank/Public/SprungWheel.h
@@ -28,6 +28,11 @@ protected:
 private:
 	void SetupConstraint();
 
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+	void ApplyForce();
+
 	// Components
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	USphereComponent* Wheel = nullptr;
@@ -40,5 +45,7 @@ private:
 
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	UPhysicsConstraintComponent* AxleWheelConstraint = nullptr;
+
+	float TotalForceMagnitudeThisFrame = 0;
 
 };
